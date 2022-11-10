1ad8a3201aa09205a009ff4818d8785f69c0f088
Mohamed Abd Elwahab Elazab Oka <93409090+MohamedAbdElwahabOka@users.noreply.github.com>
2022-11-10 14:34:01 +0200

    Add files via upload

---
+++ F. Digits Summation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    long long int a;
+    long long int b;
+    cin>>a>>b;
+    long long int A=a%10;
+    long long int B=b%10;
+ 
+    cout<<A+B;
+    
+
+    return 0;
+}

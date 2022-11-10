7c4891cdb98c72d4062d717a691ce5b0710c19d6
Mohamed Abd Elwahab Elazab Oka <93409090+MohamedAbdElwahabOka@users.noreply.github.com>
2022-11-10 14:45:40 +0200

    Add files via upload

---
+++ G. Summation from 1 to N.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    long long int a;
+    cin>>a;
+    long long int k;
+    k=((a+1)*a)/2;
+    
+ 
+    cout<<k;
+    
+
+    return 0;
+}

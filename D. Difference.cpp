95fd232027b81a93734c58f23451e00993fa7dca
Mohamed Abd Elwahab Elazab Oka <93409090+MohamedAbdElwahabOka@users.noreply.github.com>
2022-11-10 14:29:48 +0200

    Add files via upload

---
+++ D. Difference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a,b,c,d,x;
+    
+   
+    cin>>a>>b>>c>>d;
+    x=(a*b)-(c*d);
+    cout<<"Difference = "<<x ;
+       
+    
+
+    return 0;
+}

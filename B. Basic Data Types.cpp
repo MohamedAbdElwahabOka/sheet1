94509a493620b4997ef0ec88d8f322a2eaec644d
Mohamed Abd Elwahab Elazab Oka <93409090+MohamedAbdElwahabOka@users.noreply.github.com>
2022-11-10 14:28:21 +0200

    Add files via upload

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    long long b;
+    char c;
+    float f;
+    double d;
+    cin>>a>>b>>c>>f>>d;
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<c<<"\n";
+    cout<<f<<"\n";
+    cout<<d<<"\n";
+       
+    
+
+    return 0;
+}

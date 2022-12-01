76bb3a9752e29df74d58eeca8f250e3dd818025d
Mohamed Abd Elwahab Elazab Oka <93409090+MohamedAbdElwahabOka@users.noreply.github.com>
2022-12-01 14:38:25 +0200

    Create V. Comparison.cpp

---
+++ V. Comparison.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+   int a,b;
+   char c;
+   cin>>a;
+   cin>>c;
+   cin>>b;
+   
+   if(c=='<'){
+       if(a<b){
+           cout<<"Right";
+       }else{
+           cout<<"Wrong";
+       }
+   }else if(c=='>'){
+       if(a>b){
+           cout<<"Right";
+       }else{
+           cout<<"Wrong";
+       }
+   }else if(c=='='){
+       if(a==b){
+           cout<<"Right";
+       }else{
+           cout<<"Wrong";
+       }
+   }
+
+    return 0;
+}
